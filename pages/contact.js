0c0c4af2367809bcd3e555068a732d7a779f20f0
Rafa Erlangga <erlangga.rafa022@gmail.com>
2023-08-23 15:46:47 +0700

    update 23 08 23

---
+++ pages/contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/navbar.js'
+
+function Contact() {
+  return (
+    <div className='text-white scroll-smooth w-screen min-h-screen bg-gradient-to-bl from-theme to-themeSecondary inline-block transform duration-400 transition'>
+        {/* navbar */}
+        <Navbar />
+    </div>
+  )
+}
+
+export default Contact
